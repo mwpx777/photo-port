728e7b59a50ef1daf34418e19e78cba913e65817
MWPX777 <mwpx777@gmail.com>
2021-04-23 07:12:43 -0500

    finished with 20.2

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,7 +11,7 @@ function Nav() {
         },
         {
             name: 'portraits',
-            description : 'Portraits of people in my life'
+            description: 'Portraits of people in my life'
         },
         {
             name: 'food',
@@ -22,41 +22,39 @@ function Nav() {
             description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
         },
     ];
-    
-    function categorySelected(name){
+
+    function categorySelected(name) {
         console.log(`${name} clicked`)
     }
-    return(
+
+    return (
         <header data-testid="header" className="flex-row px-1">
             <h2>
-                <a href="/">
-                    <span role="img" aria-label="camera">📸</span>Oh Snap!
+                <a data-testid="link" href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about">
                             About Me
                         </a>
                     </li>
                     <li>
                         <span>Contact</span>
                     </li>
-                    {categories.map((category) =>(
-                        <li
-                            className="mx-1"
-                            key={category.name}
-                            >
-                                <span onClick={() => categorySelected(category.name)}>
-                                    {category.name}
-                                </span>
-                            </li>
+                    {categories.map((category) => (
+                        <li className="mx-1" key={category.name}>
+                            <span onClick={() => categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
                     ))}
                 </ul>
             </nav>
         </header>
-        );
+    );
 }
 
 
