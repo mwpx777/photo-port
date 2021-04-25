5261958c2f52bbff63ae301b83bf9f13122fd58f
MWPX777 <mwpx777@gmail.com>
2021-04-25 14:53:19 -0500

    finished with 20.4

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -50,7 +50,7 @@ function ContactForm() {
 
     return (
         <section>
-            <h1>Contact Me</h1>
+            <h1 data-testid="h1tag">Contact Me</h1>
             <form id="contact-form" onSubmit={handleSubmit} >
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -69,7 +69,7 @@ function ContactForm() {
                         <p className="error-text"> {errorMessage}</p>
                     </div>
                 )}
-                <button data-testid type="submit" >Submit</button>
+                <button data-testid="submitbtn" type="submit" >Submit</button>
             </form>
         </section>
     );
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,7 +7,9 @@ const categories = [
     {name: 'portraits', description: 'portraits of people in my life'}
 ]
 const mockCurrentCategory =jest.fn();
-const mockSetCurrentCategory = jest.fn
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -19,6 +21,8 @@ describe('Nav component', () => {
           categories={categories}
           setCurrentCategory={mockSetCurrentCategory}
           currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
         />);
       })
   
