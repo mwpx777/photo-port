eb897cfc562b8b2bda50fa9b3cb43b11ebda45d2
MWPX777 <mwpx777@gmail.com>
2021-04-25 11:27:54 -0500

    finished with contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,28 +2,44 @@
 
 import React, {useState} from 'react';
 
-// this is the useState hook
-// initilize the state of the hook by passing in blank user input fields
-const [formState, setFormState ] = useState({name: '', email: '', message: ''});
+
 
 function ContactForm() {
+    
+    // this is the useState hook
+    // initilize the state of the hook by passing in blank user input fields
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // these can be used to define the initial state which are empty strings
+    const {name, email, message} = formState;
+
+    function handleChange (e) {
+        // spread operator so that only the name value gets assigned
+        setFormState({...formState, [e.target.name]: e.target.value })
+        
+    }
+    // console.log(formState);
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return(
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit} >
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue = {name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="text" name="email" defaultValue={formState.name} />
+                    <input type="text"  defaultValue={email} onChange ={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue= {message} onChange={handleChange} rows="5" />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" >Submit</button>
             </form>
         </section>
         );
